f5399601efed03bb09df2a81b79b95fd28f3d63c
Narrenschlag <narrenschlag@gmail.com>
2023-12-02 15:01:26 +0100

    Added network manager class

---
+++ Modules/Walhalla/Server&Client/Network.cs
@@ -0,0 +1,32 @@
+namespace Walhalla
+{
+    public class Network
+    {
+        public TcpHandler Tcp;
+        public UdpHandler Udp;
+
+        public Network(string tcpHost, int tcpPort, string udpHost, int udpPort)
+        {
+            Tcp = new TcpHandler(tcpHost, tcpPort, receiveTcp, Disconnect);
+            Udp = new UdpHandler(udpHost, udpPort, receiveUdp);
+        }
+
+        private void receiveUdp(BufferType type, byte key, byte[] data) => Receive(type, key, data, false);
+        private void receiveTcp(BufferType type, byte key, byte[] data) => Receive(type, key, data, true);
+        public virtual void Receive(BufferType type, byte key, byte[] data, bool tcp)
+        {
+
+        }
+
+        public virtual void Send<T>(byte key, T value, bool tcp)
+        {
+            if (tcp) Tcp.send(key, value);
+            else Udp.send(key, value);
+        }
+
+        public virtual void Disconnect()
+        {
+
+        }
+    }
+}
