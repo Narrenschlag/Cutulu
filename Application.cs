978c74385707ad0e23f70ee108e2944b9cbdcb18
Narrenschlag <narrenschlag@gmail.com>
2024-02-29 02:14:35 +0100

    Added setWindowTitle

--- Application.cs
+++ Application.cs
@@ -5,17 +5,22 @@ namespace Cutulu
     public static class Application
     {
         /// <summary>
-        /// Close the application
+        /// Close the application.
         /// </summary>
         public static void Quit() => Core.Main.GetTree().Quit();
 
         /// <summary>
-        /// Open url in web browser
+        /// Open url in web browser.
         /// </summary>
         public static void OpenUrl(this string url, string requiredStart = "https://")
         {
             if (requiredStart.IsEmpty() || url.StartsWith(requiredStart))
                 OS.ShellOpen(url.Trim());
         }
+
+        /// <summary>
+        /// Define new title for application window.
+        /// </summary>
+        public static void SetWindowTitle(string newTitle, int windowId = 0) => DisplayServer.WindowSetTitle(newTitle, windowId);
     }
 }
