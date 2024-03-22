365910ce79478c648cb965e2accba699238346ff
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-03-22 15:44:11 +0100

    Added Dictionary

---
+++ Handy/SafeDictionary.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace Cutulu
+{
+    public partial class SafeDictionary<K, V> : Resource
+    {
+        private System.Collections.Generic.Dictionary<K, V> loaded;
+        public System.Collections.Generic.Dictionary<K, V> Loaded
+        {
+            get
+            {
+                if (loaded == null) Load(loaded = new());
+
+                return loaded;
+            }
+        }
+
+        public virtual void Load(System.Collections.Generic.Dictionary<K, V> dictionary) { }
+        public void Reset() => loaded = null;
+
+        public bool TryGetValue(K key, out V value) => Loaded.TryGetValue(key, out value);
+    }
+}
