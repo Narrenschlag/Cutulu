badc16f2da02e04294fa2fb25805b49c84f0581e
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-06-15 18:11:08 +0200

    Added Min,Max functions

--- Modules/Mathf/Vector2f.cs
+++ Modules/Mathf/Vector2f.cs
@@ -145,5 +145,8 @@ namespace Cutulu
                 return points[^1]; // Should never reach here if inputs are valid
             }
         }
+
+        public static Vector2I Min(this Vector2I a, Vector2I b) => new(Mathf.Min(a.X, b.X), Mathf.Min(a.Y, b.Y));
+        public static Vector2I Max(this Vector2I a, Vector2I b) => new(Mathf.Max(a.X, b.X), Mathf.Max(a.Y, b.Y));
     }
 }
