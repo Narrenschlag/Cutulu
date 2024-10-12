25e58d0ef0952073cb9a2a9f1e43b214c36a02f4
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-10-12 14:47:06 +0200

    added ignoreY param to SortByDistanceTo

--- Modules/Sortf/Vector3Sortf.cs
+++ Modules/Sortf/Vector3Sortf.cs
@@ -4,20 +4,32 @@ namespace Cutulu
 {
     public static class Vector3Sorftf
     {
-        public static Vector3[] SortByDistanceTo(this Vector3[] array, Vector3 position)
+        public static Vector3[] SortByDistanceTo(this Vector3[] array, Vector3 position, bool ignoreY = false)
         {
             for (int i = 1; i < array.Length; i++)
             {
                 var current = array[i];
-                var currentDistance = (current - position).Length();
+                var currentDistance = ignoreY ? current.toXY().DistanceTo(position.toXY()) : (current - position).Length();
                 var j = i - 1;
 
                 // Move elements of array[0..i-1], that are greater than currentDistance,
                 // to one position ahead of their current position
-                while (j >= 0 && ((array[j] - position).Length() > currentDistance))
+                if (ignoreY)
                 {
-                    array[j + 1] = array[j];
-                    j--;
+                    while (j >= 0 && array[j].toXY().DistanceTo(position.toXY()) > currentDistance)
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                    }
+                }
+
+                else
+                {
+                    while (j >= 0 && ((array[j] - position).Length() > currentDistance))
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                    }
                 }
 
                 array[j + 1] = current;
