d0a03f1de53cd3051b94fbc84902c41932e575a0
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-12-09 10:04:39 +0100

    Optimized dmod

--- Modules/Audio/DMod/DModInstance.cs
+++ Modules/Audio/DMod/DModInstance.cs
@@ -5,5 +5,15 @@ namespace Cutulu.Audio
         public float Volume { get; set; }
         public float Pitch { get; set; }
         public Godot.AudioStream Stream { get; set; }
+
+        public DModInstance() : this(default) { }
+        public DModInstance(Godot.AudioStream stream) : this(stream, 0f, 1f) { }
+        public DModInstance(Godot.AudioStream stream, float volume = 0f, float pitch = 1f)
+        {
+            Stream = stream;
+
+            Volume = volume;
+            Pitch = pitch;
+        }
     }
 }
