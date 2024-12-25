c6d4af36d82109b8501c07a0cf84a6cf8dcb2384
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-12-25 20:42:02 +0100

    Added AppData

---
+++ Modules/FileSystem/AppData.cs
@@ -0,0 +1,64 @@
+namespace Cutulu
+{
+    public static class AppData
+    {
+        public const string Path = $"{IO.USER_PATH}ApplicationData/";
+        public const string End = ".appData";
+
+        private static string GetPath(string key) => $"{Path}{key}{End}";
+
+        public static T GetAppData<T>(this string key, T defaultValue = default)
+        {
+            return TryGetAppData(key, out T output) ? output : defaultValue;
+        }
+
+        public static bool TryGetAppData<T>(this string key, out T output)
+        {
+            if (ContainsAppData(key))
+            {
+                return IO.TryRead(GetPath(key), out output, IO.FileType.Binary);
+            }
+
+            output = default;
+            return false;
+        }
+
+        public static void SetAppData(this string key, object obj)
+        {
+            if (obj == null)
+            {
+                RemoveAppData(key);
+                return;
+            }
+
+            else
+            {
+                TrySetAppData(key, obj);
+            }
+        }
+
+        public static bool TrySetAppData(this string key, object obj)
+        {
+            if (ContainsAppData(key) == false)
+            {
+                IO.Write(obj, GetPath(key), IO.FileType.Binary);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void RemoveAppData(this string key)
+        {
+            if (ContainsAppData(key))
+            {
+                IO.DeleteFile(GetPath(key));
+            }
+        }
+
+        public static bool ContainsAppData(this string key)
+        {
+            return IO.Exists(GetPath(key));
+        }
+    }
+}
