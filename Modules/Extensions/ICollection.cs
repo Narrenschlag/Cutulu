c2d5e6f222a7899cc6e6cff12249112caef971ab
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-12-25 20:42:24 +0100

    Update ICollection.cs

--- Modules/Extensions/ICollection.cs
+++ Modules/Extensions/ICollection.cs
@@ -1,6 +1,7 @@
 namespace Cutulu
 {
     using System.Collections.Generic;
+    using System.Collections;
 
     public static class CollectionExtension
     {
@@ -11,5 +12,17 @@ namespace Cutulu
 
         public static T[] ToArray<T>(this ICollection<T> collection) => ToList(collection).ToArray();
         public static List<T> ToList<T>(this ICollection<T> collection) => new(collection);
+
+        public static T[] ToArray<T>(this ICollection collection)
+        {
+            var list = new List<T>();
+
+            foreach (var item in collection)
+            {
+                if (item is T t) list.Add(t);
+            }
+
+            return list.ToArray();
+        }
     }
 }
