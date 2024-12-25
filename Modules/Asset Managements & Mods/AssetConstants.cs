4d65fedfb18cd9eb102b094662f1b09beeba0a8f
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-12-25 20:42:21 +0100

    Added constants

--- Modules/Asset Managements & Mods/AssetConstants.cs
+++ Modules/Asset Managements & Mods/AssetConstants.cs
@@ -6,5 +6,10 @@ namespace Cutulu
         public const IO.FileType FILE_TYPE = IO.FileType.Json;
         public const string ADDRESS_SEPERATOR = "::";
         public const string MOD_ENDING = ".cm";
+
+        public static readonly string[] ClassicModPaths = new[] {
+            $"{IO.PROJECT_PATH}Mods/", $"{IO.PROJECT_PATH}Assets/", $"{IO.PROJECT_PATH}Patches/",
+            $"{IO.USER_PATH}Mods/", $"{IO.USER_PATH}Assets/", $"{IO.USER_PATH}Patches/"
+        };
     }
 }
