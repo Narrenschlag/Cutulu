c2af13cac1c16bd364a865c23bbe1074b4807b27
Maximilian Schecklmann <narrenschlag@gmail.com>
2024-12-26 04:06:56 +0100

    changed struct to class to make editing possible

--- Modules/Asset Managements & Mods/AssetBookData.cs
+++ Modules/Asset Managements & Mods/AssetBookData.cs
@@ -1,9 +1,10 @@
 namespace Cutulu
 {
-    public partial struct AssetBookData
+    public partial class AssetBookData
     {
         public string Id { get; set; }
         public int DefaultPriority { get; set; }
+        public string[] Dependencies { get; set; }
         public bool ForceEnabled { get; set; }
 
         public string Author { get; set; }
@@ -11,7 +12,6 @@ namespace Cutulu
 
         public string Name { get; set; }
         public string Description { get; set; }
-        public string[] Dependencies { get; set; }
 
         public string[] DllPaths { get; set; }
         public string[] PckPaths { get; set; }
