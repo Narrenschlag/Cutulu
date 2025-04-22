a6829e41483f1de4e765916cc123cad38dd2bc22
Maximilian Schecklmann <narrenschlag@gmail.com>
2025-04-22 19:02:02 +0200

    Update Application.cs

--- Core/Application.cs
+++ Core/Application.cs
@@ -5,12 +5,12 @@ namespace Cutulu.Core
     public static class Application
     {
         /// <summary>
-        /// Close the application.
+        /// Close the application
         /// </summary>
         public static void Quit() => Nodef.Main.GetTree().Quit();
 
         /// <summary>
-        /// Open url in web browser.
+        /// Open url in web browser
         /// </summary>
         public static void OpenUrl(this string url, string requiredStart = "https://")
         {
@@ -19,7 +19,7 @@ namespace Cutulu.Core
         }
 
         /// <summary>
-        /// Define new title for application window.
+        /// Define new title for application window
         /// </summary>
         public static void SetWindowTitle(string newTitle, int windowId = 0) => DisplayServer.WindowSetTitle(newTitle, windowId);
 
@@ -30,6 +30,24 @@ namespace Cutulu.Core
         {
             set => DisplayServer.ClipboardSet(value);
             get => DisplayServer.ClipboardGet();
+        }
+
+        /// <summary>
+        /// Returns true if running in headless mode
+        /// </summary>
+        public static bool IsHeadless
+        {
+            get => DisplayServer.GetName() == "headless";
+        }
+
+        /// <summary>
+        /// Returns true if any --script-args were provided
+        /// <para>Schema: "command-to-start-application" --script-args "arg1 arg2 arg3"</para>
+        /// </summary>
+        public static bool TryGetCmdLineArgs(out string[] _args)
+        {
+            // Get all command line arguments
+            return (_args = OS.GetCmdlineArgs()).NotEmpty();
         }
     }
 }
