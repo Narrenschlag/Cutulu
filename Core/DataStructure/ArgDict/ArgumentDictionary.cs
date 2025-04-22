476ae3adf80d5ba27d9e77a447b5931476e15825
Maximilian Schecklmann <narrenschlag@gmail.com>
2025-04-22 19:02:07 +0200

    Create ArgumentDictionary.cs

---
+++ Core/DataStructure/ArgDict/ArgumentDictionary.cs
@@ -0,0 +1,33 @@
+namespace Cutulu.Core
+{
+    using System.Collections.Generic;
+
+    public static class ArgumentDictionary
+    {
+        public static Dictionary<string, string> GetDictionary(this string[] _args, char _seperator = ':')
+        {
+            var _dictionary = new Dictionary<string, string>();
+
+            if (_args.NotEmpty())
+            {
+                foreach (var _plain in _args)
+                {
+                    var _split = _plain.Split(_seperator, Constant.StringSplit);
+
+                    if (_split.Size() >= 2)
+                    {
+                        _dictionary[_split[0].ToLower()] = _split[1].ToLower();
+                    }
+                }
+            }
+
+            return _dictionary;
+        }
+
+        public static bool TryGetInt(this Dictionary<string, string> _source, string _key, out int _value)
+        {
+            _value = 0;
+            return _source.TryGetValue(_key, out _key) && int.TryParse(_key, out _value);
+        }
+    }
+}
