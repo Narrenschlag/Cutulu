9fcaba74ada4f0e577608a8fa1aaeb3cabca3fb6
Maximilian Schecklmann <narrenschlag@gmail.com>
2025-11-05 16:05:18 +0100

    Create SwapbackArray.cs

---
+++ Core/DataStructure/SwapbackArray.cs
@@ -0,0 +1,62 @@
+namespace Cutulu.Core;
+
+using System.Runtime.CompilerServices;
+using System;
+
+/// <summary>
+/// Written by Maximilian Schecklmann on 3th of Nov 2025, inspired by Nic Barker's implementation.
+/// </summary>
+public sealed class SwapbackArray<T>
+{
+    private T[] _data;
+    private int _count;
+
+    public int Count => _count;
+    public int Capacity => _data.Length;
+
+    public SwapbackArray(int capacity = 4)
+    {
+        _data = capacity == 0 ? [] : new T[capacity];
+    }
+
+    public SwapbackArray(params T[] data)
+    {
+        _data = data ?? [];
+        _count = _data.Length;
+    }
+
+    public ref T this[int index]
+    {
+        get
+        {
+            if ((uint)index >= (uint)_count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            return ref _data[index];
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Add(T item)
+    {
+        if (_count == _data.Length)
+        {
+            var newCap = Math.Max(4, _data.Length * 2);
+            Array.Resize(ref _data, newCap);
+        }
+        _data[_count++] = item;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void RemoveAt(int index)
+    {
+        if ((uint)index >= (uint)_count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        _count--;
+        if (index < _count)
+            _data[index] = _data[_count];
+        _data[_count] = default!;
+    }
+
+    public Span<T> AsSpan() => _data.AsSpan(0, _count);
+}
