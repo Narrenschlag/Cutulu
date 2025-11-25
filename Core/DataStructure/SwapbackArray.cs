4ce46e9d41afe42d66c756544c30cf9dcb791735
Maximilian Schecklmann <narrenschlag@gmail.com>
2025-11-25 14:59:27 +0100

    Update SwapbackArray.cs

--- Core/DataStructure/SwapbackArray.cs
+++ Core/DataStructure/SwapbackArray.cs
@@ -82,4 +82,17 @@ public sealed class SwapbackArray<T>
     }
 
     public Span<T> AsSpan() => _data.AsSpan(0, _count);
+
+    class Encoder : BinaryEncoder<SwapbackArray<T>>
+    {
+        public override void Encode(System.IO.BinaryWriter writer, ref object value)
+        {
+            writer.Encode(((SwapbackArray<T>)value)._data);
+        }
+
+        public override object Decode(System.IO.BinaryReader reader)
+        {
+            return new SwapbackArray<T>(reader.Decode<T[]>());
+        }
+    }
 }
