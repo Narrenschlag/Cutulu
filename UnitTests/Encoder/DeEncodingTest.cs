e12ecdca75ab22caf5b43f47cb589c4e509f6af5
Maximilian Schecklmann <narrenschlag@gmail.com>
2025-12-01 17:31:18 +0100

    Update DeEncodingTest.cs

--- UnitTests/Encoder/DeEncodingTest.cs
+++ UnitTests/Encoder/DeEncodingTest.cs
@@ -17,114 +17,119 @@ namespace Cutulu.Core.UnitTest
 
             void stepLog(string hint) => Debug.LogError($"Integration Test ___{hint}___ [{++step}/{steps}]");
 
-            try
+            for (int i = 0; i < 10; i++)
             {
-                #region Step 1
-                stepLog("MsgString");
-
-                Debug.Log($"Encoder Count: {BinaryEncoding.EncoderCount}");
-
-                var str0 = "Hello World!";
-                var buff = str0.Encode();
-
-                Debug.Log($"Encoded '{str0}' into {buff.Length} bytes");
-
-                var str1 = buff.Decode<string>();
-                Debug.Log($"{str0} => [{buff.Length} bytes] => {str1}");
-                #endregion
-
-                #region Step 2
-                stepLog("RandomClass");
-
-                var testClass0 = new TestClass0()
+                try
                 {
-                    Name = "Der Narr",
-                    HealthPoints = 69,
-                };
+                    step = 0;
 
-                buff = testClass0.Encode();
+                    #region Step 1
+                    stepLog("MsgString");
 
-                Debug.Log($"Encoded '{testClass0.Name} ({testClass0.HealthPoints} hp)' into {buff.Length} bytes");
+                    Debug.Log($"Encoder Count: {BinaryEncoding.EncoderCount}");
 
-                var testClass1 = buff.Decode<TestClass0>() ?? throw new("testClass1 could not be decoded.");
+                    var str0 = "Hello World!";
+                    var buff = str0.Encode();
 
-                Debug.Log($"'{testClass0.Name} ({testClass0.HealthPoints} hp)' => [{buff.Length} bytes] => '{testClass1.Name} ({testClass1.HealthPoints} hp)'");
-                #endregion
+                    Debug.Log($"Encoded '{str0}' into {buff.Length} bytes");
 
-                #region Step 3
-                stepLog("Vector3Encoder");
+                    var str1 = buff.Decode<string>();
+                    Debug.Log($"{str0} => [{buff.Length} bytes] => {str1}");
+                    #endregion
 
-                var vector30 = new Vector3(1, 2, -3);
+                    #region Step 2
+                    stepLog("RandomClass");
 
-                buff = vector30.Encode();
+                    var testClass0 = new TestClass0()
+                    {
+                        Name = "Der Narr",
+                        HealthPoints = 69,
+                    };
 
-                Debug.Log($"Encoded {vector30} into {buff.Length} bytes");
+                    buff = testClass0.Encode();
 
-                var vector31 = buff.Decode<Vector3>();
-                if (vector31 != vector30) throw new("vector3 cannot be decoded");
+                    Debug.Log($"Encoded '{testClass0.Name} ({testClass0.HealthPoints} hp)' into {buff.Length} bytes");
 
-                Debug.Log($"{vector30} => [{buff.Length} bytes] => {vector31}");
-                #endregion
+                    var testClass1 = buff.Decode<TestClass0>() ?? throw new("testClass1 could not be decoded.");
 
-                #region Step 4
-                stepLog("DictionaryEncoder");
+                    Debug.Log($"'{testClass0.Name} ({testClass0.HealthPoints} hp)' => [{buff.Length} bytes] => '{testClass1.Name} ({testClass1.HealthPoints} hp)'");
+                    #endregion
 
-                var dict = new System.Collections.Generic.Dictionary<string, int>()
+                    #region Step 3
+                    stepLog("Vector3Encoder");
+
+                    var vector30 = new Vector3(1, 2, -3);
+
+                    buff = vector30.Encode();
+
+                    Debug.Log($"Encoded {vector30} into {buff.Length} bytes");
+
+                    var vector31 = buff.Decode<Vector3>();
+                    if (vector31 != vector30) throw new("vector3 cannot be decoded");
+
+                    Debug.Log($"{vector30} => [{buff.Length} bytes] => {vector31}");
+                    #endregion
+
+                    #region Step 4
+                    stepLog("DictionaryEncoder");
+
+                    var dict = new System.Collections.Generic.Dictionary<string, int>()
                 {
                     { "entry0", -1 },
                     { "last", 9997 },
                 };
 
-                buff = dict.Encode();
+                    buff = dict.Encode();
 
-                Debug.Log($"Encoded {dict.GetType().Name}[{dict.Count} entries] into {buff.Length} bytes");
-                foreach (var pair in dict) Debug.LogR($"[color=magenta]> [/color]{pair.Key}: {pair.Value}");
+                    Debug.Log($"Encoded {dict.GetType().Name}[{dict.Count} entries] into {buff.Length} bytes");
+                    foreach (var pair in dict) Debug.LogR($"[color=magenta]> [/color]{pair.Key}: {pair.Value}");
 
-                if (buff.TryDecode(out System.Collections.Generic.Dictionary<string, int> dict2) == false || dict2.Count != dict.Count)
-                    throw new("Dictionary<string, int> cannot be decoded");
+                    if (buff.TryDecode(out System.Collections.Generic.Dictionary<string, int> dict2) == false || dict2.Count != dict.Count)
+                        throw new("Dictionary<string, int> cannot be decoded");
 
-                foreach (var pair in dict) Debug.LogR($"[color=seagreen]> [/color]{pair.Key}: {pair.Value}");
+                    foreach (var pair in dict) Debug.LogR($"[color=seagreen]> [/color]{pair.Key}: {pair.Value}");
 
-                Debug.LogR($"[color=seagreen]>>>[/color] Array is generic: {new int[1].GetType().IsGenericType}");
-                Debug.LogR($"[color=seagreen]>>>[/color] Array<T> is generic: {Array.Empty<TestClass0>().GetType().IsGenericType}");
-                Debug.LogR($"[color=seagreen]>>>[/color] List<T> is generic: {new System.Collections.Generic.List<int>().GetType().IsGenericType}");
+                    Debug.LogR($"[color=seagreen]>>>[/color] Array is generic: {new int[1].GetType().IsGenericType}");
+                    Debug.LogR($"[color=seagreen]>>>[/color] Array<T> is generic: {Array.Empty<TestClass0>().GetType().IsGenericType}");
+                    Debug.LogR($"[color=seagreen]>>>[/color] List<T> is generic: {new System.Collections.Generic.List<int>().GetType().IsGenericType}");
 
-                #endregion
+                    #endregion
 
-                #region Step 5
-                stepLog("(,)-Encoder");
+                    #region Step 5
+                    stepLog("(,)-Encoder");
 
-                var abc = (69, 'P');
+                    var abc = (69, 'P');
 
-                buff = abc.Encode();
+                    buff = abc.Encode();
 
-                Debug.Log($"Encoded {abc.GetType().Name} into {buff.Length} bytes");
+                    Debug.Log($"Encoded {abc.GetType().Name} into {buff.Length} bytes");
 
-                if (buff.TryDecode(out (int UID, char Name) def) == false || def != abc)
-                    throw new($"{abc.GetType().Name} cannot be decoded");
+                    if (buff.TryDecode(out (int UID, char Name) def) == false || def != abc)
+                        throw new($"{abc.GetType().Name} cannot be decoded");
 
-                Debug.Log($"Decoded into {abc.Item1}={def.UID}, {abc.Item2}={def.Name}");
+                    Debug.Log($"Decoded into {abc.Item1}={def.UID}, {abc.Item2}={def.Name}");
 
-                var ghi = (69, '+', new Vector2(-101, 101), "Name");
+                    var ghi = (69, '+', new Vector2(-101, 101), "Name");
 
-                buff = ghi.Encode();
+                    buff = ghi.Encode();
 
-                Debug.Log($"Encoded {ghi.GetType().Name} into {buff.Length} bytes");
+                    Debug.Log($"Encoded {ghi.GetType().Name} into {buff.Length} bytes");
 
-                if (buff.TryDecode(out (int UID, char Char, Vector2 V2, string Name) jkl) == false || jkl != ghi)
-                    throw new($"{ghi.GetType().Name} cannot be decoded");
+                    if (buff.TryDecode(out (int UID, char Char, Vector2 V2, string Name) jkl) == false || jkl != ghi)
+                        throw new($"{ghi.GetType().Name} cannot be decoded");
 
-                Debug.Log($"Decoded into {ghi.Item1}={jkl.UID}, {ghi.Item2}={jkl.Char}, {ghi.Item3}={jkl.V2}, {ghi.Item4}={jkl.Name}");
+                    Debug.Log($"Decoded into {ghi.Item1}={jkl.UID}, {ghi.Item2}={jkl.Char}, {ghi.Item3}={jkl.V2}, {ghi.Item4}={jkl.Name}");
 
-                #endregion
-            }
+                    #endregion
+                }
 
-            catch (Exception ex)
-            {
-                time.Stop();
-                Debug.LogError($"Failed in {time.ElapsedMilliseconds} ms [{step}/{steps}] {ex.Message}\n{ex.StackTrace}");
-                Application.Quit();
-                return;
+                catch (Exception ex)
+                {
+                    time.Stop();
+                    Debug.LogError($"Failed in {time.ElapsedMilliseconds} ms [{step}/{steps}] {ex.Message}\n{ex.StackTrace}");
+                    Application.Quit();
+                    return;
+                }
             }
 
             //finally
