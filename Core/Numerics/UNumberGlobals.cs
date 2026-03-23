3d4e47c9751674ba7e072741eed9b0a6b91ef02f
Maximilian Schecklmann <narrenschlag@gmail.com>
2026-03-23 07:33:52 +0100

    Update UNumberGlobals.cs

--- Core/Numerics/UNumberGlobals.cs
+++ Core/Numerics/UNumberGlobals.cs
@@ -1,4 +1,4 @@
-global using UNumber8 = Cutulu.Core.UNumber<byte>;
+global using UNumber8 = byte;  //Cutulu.Core.UNumber<byte>; -> byte is already only one byte of size
 global using UNumber16 = Cutulu.Core.UNumber<ushort>;
 global using UNumber32 = Cutulu.Core.UNumber<uint>;
 global using UNumber64 = Cutulu.Core.UNumber<ulong>;
