91482d1e07ffc13ec893c83da4695a2134f95d00
Maximilian Schecklmann <narrenschlag@gmail.com>
2026-05-12 19:54:15 +0200

    Added named pipe

---
+++ Network/NamedPipe.cs
@@ -0,0 +1,118 @@
+namespace Cutulu.Network;
+
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using Cutulu.Core;
+using System.IO;
+using System;
+
+public static class NamedPipe
+{
+    // Server
+    public static async Task<Socket> StartHost(string name, CancellationToken token = default, bool startListening = true)
+    {
+        var server = new NamedPipeServerStream(
+            name,
+            PipeDirection.InOut,
+            maxNumberOfServerInstances: 1,
+            transmissionMode: PipeTransmissionMode.Byte
+        );
+
+        try
+        {
+            await server.WaitForConnectionAsync(token);
+        }
+        catch
+        {
+            await server.DisposeAsync();
+            throw;
+        }
+
+        var pipe = new Socket(server);
+        if (startListening) pipe.StartListening();
+        return pipe;
+    }
+
+    // Client
+    public static async Task<Socket> Connect(string name, string serverName = ".", CancellationToken token = default, bool startListening = true)
+    {
+        var client = new NamedPipeClientStream(
+            serverName, // "." = local machine
+            name,
+            PipeDirection.InOut,
+            PipeOptions.Asynchronous
+        );
+
+        await client.ConnectAsync(token);
+
+        var pipe = new Socket(client);
+        if (startListening) pipe.StartListening();
+
+        return pipe;
+    }
+
+    public class Socket(PipeStream stream)
+    {
+        public readonly PipeStream Stream = stream;
+        private readonly BinaryWriter Writer = new(stream);
+
+        public readonly Notification<UNumber32, LocalDecoder> Received = new();
+        public readonly Notification Disconnected = new();
+        private CancellationTokenSource? TokenSource;
+        private Task? Listener;
+
+        public void Send(UNumber32 key, object obj)
+        {
+            var encoder = new LocalEncoder();
+            encoder.Write(key);
+            encoder.Write(obj);
+
+            byte[] buffer = encoder.GetBuffer();
+            Writer.Write(buffer.Length);
+            Writer.Write(buffer);
+            Writer.Flush();
+        }
+
+        public void StartListening()
+        {
+            if (Listener is not null) return;
+            TokenSource = new CancellationTokenSource();
+            Listener = ListenTask(TokenSource.Token);
+        }
+
+        public void StopListening()
+        {
+            if (Listener is null) return;
+            TokenSource?.Cancel();
+            Listener.Dispose();
+            Listener = null;
+        }
+
+        private async Task ListenTask(CancellationToken token)
+        {
+            byte[] lengthBuffer = new byte[4];
+            byte[] dataBuffer;
+
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    await Stream.ReadExactlyAsync(lengthBuffer.AsMemory(), token);
+                    if (token.IsCancellationRequested) break;
+
+                    int length = BitConverter.ToInt32(lengthBuffer, 0);
+                    dataBuffer = new byte[length];
+
+                    await Stream.ReadExactlyAsync(dataBuffer.AsMemory(), token);
+                    if (token.IsCancellationRequested) break;
+
+                    var decoder = new LocalDecoder(dataBuffer);
+                    Received.Invoke(decoder.Decode<UNumber32>(), decoder);
+                }
+            }
+            catch (OperationCanceledException) { /* clean shutdown */ }
+            catch (EndOfStreamException) { /* pipe closed remotely */ Disconnected?.Invoke(); }
+            catch (IOException) { /* pipe broken */ Disconnected?.Invoke(); }
+        }
+    }
+}
